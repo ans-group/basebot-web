930baab263d4966649f37d4234fb43df970703e4
ceuk <ceuk.dev@gmail.com>
2020-04-22 11:29:21 +0100

    improve message formatting

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import Linkify from 'react-linkify'
 import classNames from 'classnames'
-import { markdown } from 'markdown'
+import Markdown from 'react-markdown'
 import styles from './Messages.module.scss'
 
-const Message = ({from, text, style}) => {
+const Message = ({ from, text, style }) => {
   const classes = classNames(
     styles.message,
-    {[styles.fromUser]: from === 'user'}
+    { [styles.fromUser]: from === 'user' }
   )
 
   return (
-    <div className={classes} style={style} dangerouslySetInnerHTML={{__html: markdown.toHTML(text)}} />
+    <Linkify properties={{
+      target: '_blank',
+      rel: 'noopener noreferrer'
+    }}>
+      <Markdown
+        className={classes}
+        linkTarget='_blank'
+        style={style}
+        source={text}
+        escapeHtml={false}
+        disallowedTypes={['html']}
+      />
+    </Linkify>
   )
 }
 
